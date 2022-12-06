e709d586b1980f1faf494b357d1fdaa52796258a
Jeffrey Palermo <jeffrey@clear-measure.com>
2022-12-06 16:52:24 -0600

    refactored

--- src/Core/Model/TrimmedDecimal.cs
+++ src/Core/Model/TrimmedDecimal.cs
@@ -1,163 +1,171 @@
-﻿using System.Numerics;
+﻿namespace ProgrammingWithPalermo.ChurchBulletin.Core.Model;
 
-namespace ProgrammingWithPalermo.ChurchBulletin.Core.Model
+public class TrimmedDecimal : IComparable
 {
-    public class TrimmedDecimal : IComparable
+    public TrimmedDecimal(decimal? val)
     {
-        
-        public decimal Value { get; set; }
+        Value = val.GetValueOrDefault();
+    }
 
-        public TrimmedDecimal(decimal? val)
-        {
-            Value = val.GetValueOrDefault();
-        }
+    public decimal Value { get; set; }
 
-        public override string ToString()
-        {
-            return Value.ToString("0.######");
-        }
-        public override bool Equals(object obj)
-        {
-            if (obj == null)
-            {
-                return false;
-            }
-            if (obj is TrimmedDecimal)
-            {
-                return this.CompareTo(obj) == 0;
-            }
-            return false;
-        }
-        public override int GetHashCode()
-        {
-            return Value.GetHashCode();
-        }
-        public int CompareTo(object obj)
-        {
-            if (obj == null)
-            {
-                return 1;
-            }
-            if (obj is TrimmedDecimal)
-            {
-                return this.CompareTo((TrimmedDecimal) obj);
-            }
+    public int CompareTo(object obj)
+    {
+        if (obj == null) return 1;
+        if (obj is TrimmedDecimal) return CompareTo((TrimmedDecimal)obj);
 
-            throw new Exception($"Can't compare to instance of {obj.GetType()}");
-        }
-        public int CompareTo(TrimmedDecimal other)
-        {
-            if (other == null)
-            {
-                return 1;
-            }
-            if (this.Value < other.Value)
-            {
-                return -1;
-            }
-            if (this.Value > other.Value)
-            {
-                return 1;
-            }
-            return 0;
-        }
+        throw new Exception($"Can't compare to instance of {obj.GetType()}");
+    }
 
-        public static bool operator ==(TrimmedDecimal left, TrimmedDecimal right)
-        {
-            if ((object) left == null)
-            {
-                return (object) right == null;
-            }
+    public override string ToString()
+    {
+        return Value.ToString("0.######");
+    }
 
-            return left.Equals(right);
-        }
-        public static bool operator !=(TrimmedDecimal left, TrimmedDecimal right)
-        {
-            return !(left == right);
-        }
-        public static bool operator >(TrimmedDecimal left, TrimmedDecimal right)
-        {
-            if (left == null)
-            {
-                return false;
-            }
+    public override bool Equals(object obj)
+    {
+        if (obj == null) return false;
+        if (obj is TrimmedDecimal) return CompareTo(obj) == 0;
+        return false;
+    }
 
-            return left.CompareTo(right) > 0;
-        }
-        public static bool operator <(TrimmedDecimal left, TrimmedDecimal right)
-        {
-            if (left == null)
-            {
-                return right != null;
-            }
+    public override int GetHashCode()
+    {
+        return Value.GetHashCode();
+    }
 
-            return left.CompareTo(right) < 0;
-        }
-        public static bool operator >=(TrimmedDecimal left, TrimmedDecimal right)
-        {
-            return left > right || left == right;
-        }
-        public static bool operator <=(TrimmedDecimal left, TrimmedDecimal right)
-        {
-            return left < right || left == right;
+    public int CompareTo(TrimmedDecimal other)
+    {
+        if (other == null) return 1;
+        if (Value < other.Value) return -1;
+        if (Value > other.Value) return 1;
+        return 0;
+    }
 
-        }
+    public static bool operator ==(TrimmedDecimal left, TrimmedDecimal right)
+    {
+        if ((object)left == null) return (object)right == null;
 
-        public static bool operator ==(TrimmedDecimal left, decimal right)
-        {
-            return left == new TrimmedDecimal(right);
-        }
-        public static bool operator !=(TrimmedDecimal left, decimal right)
-        {
-            return left != new TrimmedDecimal(right);
-        }
-        public static bool operator >(TrimmedDecimal left, decimal right)
-        {
-            return left > new TrimmedDecimal(right);
-        }
-        public static bool operator <(TrimmedDecimal left, decimal right)
-        {
-            return left < new TrimmedDecimal(right);
-        }
-        public static bool operator >=(TrimmedDecimal left, decimal right)
-        {
-            return left >= new TrimmedDecimal(right);
-        }
-        public static bool operator <=(TrimmedDecimal left, decimal right)
-        {
-            return left <= new TrimmedDecimal(right);
-        }
-        //=========================================
-        public static bool operator ==(decimal left, TrimmedDecimal right)
-        {
-            return new TrimmedDecimal(left) == right;
-        }
-        public static bool operator !=(decimal left, TrimmedDecimal right)
-        {
-            return new TrimmedDecimal(left) != right;
-        }
-        public static bool operator >(decimal left, TrimmedDecimal right)
-        {
-            return new TrimmedDecimal(left) > right;
-        }
-        public static bool operator <(decimal left, TrimmedDecimal right)
-        {
-            return new TrimmedDecimal(left) < right;
-        }
-        public static bool operator >=(decimal left, TrimmedDecimal right)
-        {
-            return new TrimmedDecimal(left) >= right;
-        }
-        public static bool operator <=(decimal left, TrimmedDecimal right)
-        {
-            return new TrimmedDecimal(left) <= right;
-        }
+        return left.Equals(right);
+    }
 
-        public static implicit operator decimal(TrimmedDecimal num) => num == null ? 0 : num.Value;
-        public static implicit operator float(TrimmedDecimal num) => num == null ? 0 : (float)num.Value;
-        public static implicit operator int(TrimmedDecimal num) => num == null ? 0 : (int)num.Value;
-        public static implicit operator TrimmedDecimal(decimal num) => new(num);
-        public static implicit operator TrimmedDecimal(float num) => new((decimal)num);
-        public static implicit operator TrimmedDecimal(int num) => new(num);
+    public static bool operator !=(TrimmedDecimal left, TrimmedDecimal right)
+    {
+        return !(left == right);
+    }
+
+    public static bool operator >(TrimmedDecimal left, TrimmedDecimal right)
+    {
+        if (left == null) return false;
+
+        return left.CompareTo(right) > 0;
+    }
+
+    public static bool operator <(TrimmedDecimal left, TrimmedDecimal right)
+    {
+        if (left == null) return right != null;
+
+        return left.CompareTo(right) < 0;
+    }
+
+    public static bool operator >=(TrimmedDecimal left, TrimmedDecimal right)
+    {
+        return left > right || left == right;
+    }
+
+    public static bool operator <=(TrimmedDecimal left, TrimmedDecimal right)
+    {
+        return left < right || left == right;
+    }
+
+    public static bool operator ==(TrimmedDecimal left, decimal right)
+    {
+        return left == new TrimmedDecimal(right);
+    }
+
+    public static bool operator !=(TrimmedDecimal left, decimal right)
+    {
+        return left != new TrimmedDecimal(right);
+    }
+
+    public static bool operator >(TrimmedDecimal left, decimal right)
+    {
+        return left > new TrimmedDecimal(right);
+    }
+
+    public static bool operator <(TrimmedDecimal left, decimal right)
+    {
+        return left < new TrimmedDecimal(right);
+    }
+
+    public static bool operator >=(TrimmedDecimal left, decimal right)
+    {
+        return left >= new TrimmedDecimal(right);
+    }
+
+    public static bool operator <=(TrimmedDecimal left, decimal right)
+    {
+        return left <= new TrimmedDecimal(right);
+    }
+
+    //=========================================
+    public static bool operator ==(decimal left, TrimmedDecimal right)
+    {
+        return new TrimmedDecimal(left) == right;
+    }
+
+    public static bool operator !=(decimal left, TrimmedDecimal right)
+    {
+        return new TrimmedDecimal(left) != right;
+    }
+
+    public static bool operator >(decimal left, TrimmedDecimal right)
+    {
+        return new TrimmedDecimal(left) > right;
+    }
+
+    public static bool operator <(decimal left, TrimmedDecimal right)
+    {
+        return new TrimmedDecimal(left) < right;
+    }
+
+    public static bool operator >=(decimal left, TrimmedDecimal right)
+    {
+        return new TrimmedDecimal(left) >= right;
+    }
+
+    public static bool operator <=(decimal left, TrimmedDecimal right)
+    {
+        return new TrimmedDecimal(left) <= right;
+    }
+
+    public static implicit operator decimal(TrimmedDecimal num)
+    {
+        return num == null ? 0 : num.Value;
+    }
+
+    public static implicit operator float(TrimmedDecimal num)
+    {
+        return num == null ? 0 : (float)num.Value;
+    }
+
+    public static implicit operator int(TrimmedDecimal num)
+    {
+        return num == null ? 0 : (int)num.Value;
+    }
+
+    public static implicit operator TrimmedDecimal(decimal num)
+    {
+        return new(num);
+    }
+
+    public static implicit operator TrimmedDecimal(float num)
+    {
+        return new((decimal)num);
+    }
+
+    public static implicit operator TrimmedDecimal(int num)
+    {
+        return new(num);
     }
 }
--- src/UnitTests/TrimmedDecimalTester.cs
+++ src/UnitTests/TrimmedDecimalTester.cs
@@ -1,206 +1,231 @@
 ﻿using ProgrammingWithPalermo.ChurchBulletin.Core.Model;
 using Shouldly;
 
-namespace ProgrammingWithPalermo.ChurchBulletin.UnitTests
+namespace ProgrammingWithPalermo.ChurchBulletin.UnitTests;
+
+[TestFixture]
+public class TrimmedDecimalTester
 {
-    [TestFixture]
-    public class TrimmedDecimalTester
+    [Test]
+    public void Scratch()
     {
-        [Test]
-        public void ShouldRenderWithoutTrailingZeroes()
+        decimal val = 8.230000m;
+        TrimmedDecimal trimmedVal = val;
+        Console.WriteLine(trimmedVal.ToString());
+        Console.WriteLine(trimmedVal.ToString());
+        Console.WriteLine(trimmedVal.ToString());
+        Console.WriteLine(trimmedVal.ToString());
+    }
+
+    [Test]
+    public void ShouldRenderWithoutTrailingZeroes()
+    {
+        TrimmedDecimal[] numbers =
         {
-            TrimmedDecimal[] numbers = new TrimmedDecimal[] 
-            {
-                1.123456000000000000m,
-                2.000000m,
-                3.100000m,
-                4.120000m,
-                5.123000m,
-                6.123400m,
-                7.123450m,
-                8.123456m
-            };
-            AssertDecimalIsShortened(numbers);
-        }
+            1.123456000000000000m,
+            2.000000m,
+            3.100000m,
+            4.120000m,
+            5.123000m,
+            6.123400m,
+            7.123450m,
+            8.123456m
+        };
+        AssertDecimalIsShortened(numbers);
+    }
 
-        [Test]
-        public void ShouldCompareOtherDecimalIdNumber()
+    [Test]
+    public void ShouldCompareOtherDecimalIdNumber()
+    {
+        TrimmedDecimal baseNumber = 1.123400m;
+        TrimmedDecimal copyOfBase = 1.1234m;
+        TrimmedDecimal largerThanBase = 9.999956m;
+
+        //same value
+        baseNumber.Equals(copyOfBase).ShouldBeTrue();
+        baseNumber.CompareTo(copyOfBase).ShouldBe(0);
+        (baseNumber == copyOfBase).ShouldBeTrue();
+        (baseNumber != copyOfBase).ShouldBeFalse();
+        (baseNumber >= copyOfBase).ShouldBeTrue();
+        (baseNumber <= copyOfBase).ShouldBeTrue();
+        (baseNumber > copyOfBase).ShouldBeFalse();
+        (baseNumber < copyOfBase).ShouldBeFalse();
+
+        //different value
+        baseNumber.Equals(largerThanBase).ShouldBeFalse();
+        baseNumber.CompareTo(largerThanBase).ShouldBeLessThan(0);
+        largerThanBase.CompareTo(baseNumber).ShouldBeGreaterThan(0);
+        (baseNumber == largerThanBase).ShouldBeFalse();
+        (baseNumber != largerThanBase).ShouldBeTrue();
+        (baseNumber >= largerThanBase).ShouldBeFalse();
+        (baseNumber > largerThanBase).ShouldBeFalse();
+        (baseNumber <= largerThanBase).ShouldBeTrue();
+        (baseNumber < largerThanBase).ShouldBeTrue();
+        (largerThanBase >= baseNumber).ShouldBeTrue();
+        (largerThanBase > baseNumber).ShouldBeTrue();
+        (largerThanBase <= baseNumber).ShouldBeFalse();
+        (largerThanBase < baseNumber).ShouldBeFalse();
+    }
+
+    [Test]
+    public void ShouldCompareDecimalValue()
+    {
+        var baseAsValue = 1.1234m;
+        TrimmedDecimal baseNumber = baseAsValue;
+        var largerThanBaseAsValue = 9.9995m;
+        TrimmedDecimal largerThanBase = largerThanBaseAsValue;
+
+        //same value
+        (baseNumber == baseAsValue).ShouldBeTrue();
+        (baseNumber != baseAsValue).ShouldBeFalse();
+        (baseNumber >= baseAsValue).ShouldBeTrue();
+        (baseNumber <= baseAsValue).ShouldBeTrue();
+        (baseNumber > baseAsValue).ShouldBeFalse();
+        (baseNumber < baseAsValue).ShouldBeFalse();
+        (baseAsValue == baseNumber).ShouldBeTrue();
+        (baseAsValue != baseNumber).ShouldBeFalse();
+        (baseAsValue >= baseNumber).ShouldBeTrue();
+        (baseAsValue <= baseNumber).ShouldBeTrue();
+        (baseAsValue > baseNumber).ShouldBeFalse();
+        (baseAsValue < baseNumber).ShouldBeFalse();
+
+        //different value
+        (baseNumber == largerThanBaseAsValue).ShouldBeFalse();
+        (baseNumber != largerThanBaseAsValue).ShouldBeTrue();
+        (baseNumber >= largerThanBaseAsValue).ShouldBeFalse();
+        (baseNumber > largerThanBaseAsValue).ShouldBeFalse();
+        (baseNumber <= largerThanBaseAsValue).ShouldBeTrue();
+        (baseNumber < largerThanBaseAsValue).ShouldBeTrue();
+        (largerThanBase == baseAsValue).ShouldBeFalse();
+        (largerThanBase != baseAsValue).ShouldBeTrue();
+        (largerThanBase >= baseAsValue).ShouldBeTrue();
+        (largerThanBase > baseAsValue).ShouldBeTrue();
+        (largerThanBase <= baseAsValue).ShouldBeFalse();
+        (largerThanBase < baseAsValue).ShouldBeFalse();
+
+        (baseAsValue == largerThanBase).ShouldBeFalse();
+        (baseAsValue != largerThanBase).ShouldBeTrue();
+        (baseAsValue >= largerThanBase).ShouldBeFalse();
+        (baseAsValue > largerThanBase).ShouldBeFalse();
+        (baseAsValue <= largerThanBase).ShouldBeTrue();
+        (baseAsValue < largerThanBase).ShouldBeTrue();
+        (largerThanBaseAsValue == baseNumber).ShouldBeFalse();
+        (largerThanBaseAsValue != baseNumber).ShouldBeTrue();
+        (largerThanBaseAsValue >= baseNumber).ShouldBeTrue();
+        (largerThanBaseAsValue > baseNumber).ShouldBeTrue();
+        (largerThanBaseAsValue <= baseNumber).ShouldBeFalse();
+        (largerThanBaseAsValue < baseNumber).ShouldBeFalse();
+    }
+
+    [Test]
+    public void ShouldCompareNull()
+    {
+        TrimmedDecimal nullNum1 = null;
+        TrimmedDecimal nullNum2 = null;
+        TrimmedDecimal setNum = 5.234m;
+        (nullNum1 == nullNum2).ShouldBeTrue();
+        (nullNum1 != nullNum2).ShouldBeFalse();
+        (nullNum1 > nullNum2).ShouldBeFalse();
+        (nullNum1 < nullNum2).ShouldBeFalse();
+        (nullNum1 >= nullNum2).ShouldBeTrue();
+        (nullNum1 <= nullNum2).ShouldBeTrue();
+
+        (nullNum2 == nullNum1).ShouldBeTrue();
+        (nullNum2 != nullNum1).ShouldBeFalse();
+        (nullNum2 > nullNum1).ShouldBeFalse();
+        (nullNum2 < nullNum1).ShouldBeFalse();
+        (nullNum2 >= nullNum1).ShouldBeTrue();
+        (nullNum2 <= nullNum1).ShouldBeTrue();
+
+        (nullNum1 == setNum).ShouldBeFalse();
+        (nullNum1 != setNum).ShouldBeTrue();
+        (nullNum1 > setNum).ShouldBeFalse();
+        (nullNum1 < setNum).ShouldBeTrue();
+        (nullNum1 >= setNum).ShouldBeFalse();
+        (nullNum1 <= setNum).ShouldBeTrue();
+
+
+        (setNum == nullNum1).ShouldBeFalse();
+        (setNum != nullNum1).ShouldBeTrue();
+        (setNum > nullNum1).ShouldBeTrue();
+        (setNum < nullNum1).ShouldBeFalse();
+        (setNum >= nullNum1).ShouldBeTrue();
+        (setNum <= nullNum1).ShouldBeFalse();
+    }
+
+    [Test]
+    public void ShouldHaveSameHashCodeForSameValue()
+    {
+        TrimmedDecimal baseNum = 6.123000m;
+        TrimmedDecimal copyOfBase = 6.123m;
+        TrimmedDecimal differentNum = 1.87m;
+        baseNum.GetHashCode().ShouldBe(copyOfBase.GetHashCode());
+        baseNum.GetHashCode().ShouldNotBe(differentNum.GetHashCode());
+    }
+
+    [Test]
+    public void ShouldConvertToFloat()
+    {
+        float number = new TrimmedDecimal(123m);
+        number.ShouldBe<float>(new TrimmedDecimal(123m));
+    }
+
+    [Test]
+    public void ShouldConvertFromFloat()
+    {
+        TrimmedDecimal number = 123f;
+        number.ShouldBe<TrimmedDecimal>(123f);
+    }
+
+    [Test]
+    public void ShouldConvertToInt32()
+    {
+        int number = new TrimmedDecimal(123);
+        number.ShouldBe<int>(new TrimmedDecimal(123));
+    }
+
+    [Test]
+    public void ShouldConvertFromInt32()
+    {
+        TrimmedDecimal number = 123;
+        number.ShouldBe<TrimmedDecimal>(123);
+    }
+
+    [Test]
+    public void ShouldConvertToDecimal()
+    {
+        decimal number = new TrimmedDecimal(123m);
+        number.ShouldBe<decimal>(new TrimmedDecimal(123m));
+    }
+
+    [Test]
+    public void ShouldConvertFromDecimal()
+    {
+        TrimmedDecimal number = 123m;
+        number.ShouldBe<TrimmedDecimal>(123m);
+    }
+
+    private void AssertDecimalIsShortened(TrimmedDecimal[] numbers)
+    {
+        foreach (var number in numbers)
         {
-            TrimmedDecimal baseNumber = 1.123400m;
-            TrimmedDecimal copyOfBase = 1.1234m;
-            TrimmedDecimal largerThanBase = 9.999956m;
-            
-            //same value
-            baseNumber.Equals(copyOfBase).ShouldBeTrue();
-            baseNumber.CompareTo(copyOfBase).ShouldBe(0);
-            (baseNumber == copyOfBase).ShouldBeTrue();
-            (baseNumber != copyOfBase).ShouldBeFalse();
-            (baseNumber >= copyOfBase).ShouldBeTrue();
-            (baseNumber <= copyOfBase).ShouldBeTrue();
-            (baseNumber > copyOfBase).ShouldBeFalse();
-            (baseNumber < copyOfBase).ShouldBeFalse();
-            
-            //different value
-            baseNumber.Equals(largerThanBase).ShouldBeFalse();
-            baseNumber.CompareTo(largerThanBase).ShouldBeLessThan(0);
-            largerThanBase.CompareTo(baseNumber).ShouldBeGreaterThan(0);
-            (baseNumber == largerThanBase).ShouldBeFalse();
-            (baseNumber != largerThanBase).ShouldBeTrue();
-            (baseNumber >= largerThanBase).ShouldBeFalse();
-            (baseNumber > largerThanBase).ShouldBeFalse();
-            (baseNumber <= largerThanBase).ShouldBeTrue();
-            (baseNumber < largerThanBase).ShouldBeTrue();
-            (largerThanBase >= baseNumber).ShouldBeTrue();
-            (largerThanBase > baseNumber).ShouldBeTrue();
-            (largerThanBase <= baseNumber).ShouldBeFalse();
-            (largerThanBase < baseNumber).ShouldBeFalse();
+            var combinedMessage = number + " did not trim trailing 0's";
+            number.ToString().Last().ShouldNotBe('0', combinedMessage);
         }
-        
-        [Test]
-        public void ShouldCompareDecimalValue()
-        {
-            decimal baseAsValue = 1.1234m;
-            TrimmedDecimal baseNumber = baseAsValue;
-            decimal largerThanBaseAsValue = 9.9995m;
-            TrimmedDecimal largerThanBase = largerThanBaseAsValue;
-            
-            //same value
-            (baseNumber == baseAsValue).ShouldBeTrue();
-            (baseNumber != baseAsValue).ShouldBeFalse();
-            (baseNumber >= baseAsValue).ShouldBeTrue();
-            (baseNumber <= baseAsValue).ShouldBeTrue();
-            (baseNumber > baseAsValue).ShouldBeFalse();
-            (baseNumber < baseAsValue).ShouldBeFalse();
-            (baseAsValue == baseNumber).ShouldBeTrue();
-            (baseAsValue != baseNumber).ShouldBeFalse();
-            (baseAsValue >= baseNumber).ShouldBeTrue();
-            (baseAsValue <= baseNumber).ShouldBeTrue();
-            (baseAsValue > baseNumber).ShouldBeFalse();
-            (baseAsValue < baseNumber).ShouldBeFalse();
+    }
+}
 
-            //different value
-            (baseNumber == largerThanBaseAsValue).ShouldBeFalse();
-            (baseNumber != largerThanBaseAsValue).ShouldBeTrue();
-            (baseNumber >= largerThanBaseAsValue).ShouldBeFalse();
-            (baseNumber > largerThanBaseAsValue).ShouldBeFalse();
-            (baseNumber <= largerThanBaseAsValue).ShouldBeTrue();
-            (baseNumber < largerThanBaseAsValue).ShouldBeTrue();
-            (largerThanBase == baseAsValue).ShouldBeFalse();
-            (largerThanBase != baseAsValue).ShouldBeTrue();
-            (largerThanBase >= baseAsValue).ShouldBeTrue();
-            (largerThanBase > baseAsValue).ShouldBeTrue();
-            (largerThanBase <= baseAsValue).ShouldBeFalse();
-            (largerThanBase < baseAsValue).ShouldBeFalse();
+public class UIComponent
+{
+    private readonly string _val;
 
-            (baseAsValue == largerThanBase).ShouldBeFalse();
-            (baseAsValue != largerThanBase).ShouldBeTrue();
-            (baseAsValue >= largerThanBase).ShouldBeFalse();
-            (baseAsValue > largerThanBase).ShouldBeFalse();
-            (baseAsValue <= largerThanBase).ShouldBeTrue();
-            (baseAsValue < largerThanBase).ShouldBeTrue();
-            (largerThanBaseAsValue == baseNumber).ShouldBeFalse();
-            (largerThanBaseAsValue != baseNumber).ShouldBeTrue();
-            (largerThanBaseAsValue >= baseNumber).ShouldBeTrue();
-            (largerThanBaseAsValue > baseNumber).ShouldBeTrue();
-            (largerThanBaseAsValue <= baseNumber).ShouldBeFalse();
-            (largerThanBaseAsValue < baseNumber).ShouldBeFalse();
-        }
+    public UIComponent(decimal val)
+    {
+        _val = $"{val:0.######}";
+    }
 
-        [Test]
-        public void ShouldCompareNull()
-        {
-            TrimmedDecimal nullNum1 = null;
-            TrimmedDecimal nullNum2 = null;
-            TrimmedDecimal setNum = 5.234m;
-            (nullNum1 == nullNum2).ShouldBeTrue();
-            (nullNum1 != nullNum2).ShouldBeFalse();
-            (nullNum1 > nullNum2).ShouldBeFalse();
-            (nullNum1 < nullNum2).ShouldBeFalse();
-            (nullNum1 >= nullNum2).ShouldBeTrue();
-            (nullNum1 <= nullNum2).ShouldBeTrue();
-
-            (nullNum2 == nullNum1).ShouldBeTrue();
-            (nullNum2 != nullNum1).ShouldBeFalse();
-            (nullNum2 > nullNum1).ShouldBeFalse();
-            (nullNum2 < nullNum1).ShouldBeFalse();
-            (nullNum2 >= nullNum1).ShouldBeTrue();
-            (nullNum2 <= nullNum1).ShouldBeTrue();
-
-            (nullNum1 == setNum).ShouldBeFalse();
-            (nullNum1 != setNum).ShouldBeTrue();
-            (nullNum1 > setNum).ShouldBeFalse();
-            (nullNum1 < setNum).ShouldBeTrue();
-            (nullNum1 >= setNum).ShouldBeFalse();
-            (nullNum1 <= setNum).ShouldBeTrue();
-
-
-            (setNum == nullNum1).ShouldBeFalse();
-            (setNum != nullNum1).ShouldBeTrue();
-            (setNum > nullNum1).ShouldBeTrue();
-            (setNum < nullNum1).ShouldBeFalse();
-            (setNum >= nullNum1).ShouldBeTrue();
-            (setNum <= nullNum1).ShouldBeFalse();
-        }
-
-        [Test]
-        public void ShouldHaveSameHashCodeForSameValue()
-        {
-            TrimmedDecimal baseNum = 6.123000m;
-            TrimmedDecimal copyOfBase = 6.123m;
-            TrimmedDecimal differentNum = 1.87m;
-            baseNum.GetHashCode().ShouldBe(copyOfBase.GetHashCode());
-            baseNum.GetHashCode().ShouldNotBe(differentNum.GetHashCode());
-        }
-
-        [Test]
-        public void ShouldConvertToFloat()
-        {
-            float number = new TrimmedDecimal(123m);
-            number.ShouldBe<float>(new TrimmedDecimal(123m));
-        }
-
-        [Test]
-        public void ShouldConvertFromFloat()
-        {
-            TrimmedDecimal number = 123f;
-            number.ShouldBe<TrimmedDecimal>(123f);
-        }
-
-        [Test]
-        public void ShouldConvertToInt32()
-        {
-            int number = new TrimmedDecimal(123);
-            number.ShouldBe<int>(new TrimmedDecimal(123));
-        }
-
-        [Test]
-        public void ShouldConvertFromInt32()
-        {
-            TrimmedDecimal number = 123;
-            number.ShouldBe<TrimmedDecimal>(123);
-        }
-
-        [Test]
-        public void ShouldConvertToDecimal()
-        {
-            decimal number = new TrimmedDecimal(123m);
-            number.ShouldBe<decimal>(new TrimmedDecimal(123m));
-        }
-
-        [Test]
-        public void ShouldConvertFromDecimal()
-        {
-            TrimmedDecimal number = 123m;
-            number.ShouldBe<TrimmedDecimal>(123m);
-        }
-
-        private void AssertDecimalIsShortened(TrimmedDecimal[] numbers)
-        {
-            foreach (var number in numbers)
-            {
-                string combinedMessage = number + " did not trim trailing 0's";
-                number.ToString().Last().ShouldNotBe('0', combinedMessage);
-            }
-        }
+    public override string ToString()
+    {
+        return _val;
     }
 }
