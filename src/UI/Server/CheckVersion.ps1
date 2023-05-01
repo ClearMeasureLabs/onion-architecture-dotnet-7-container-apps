35e686aea1d53c541ad65fd91d7a33abb00bf350
Dale Marttinen <dmarttinen@gmail.com>
2023-05-01 12:57:05 -0700

    Moved scripts, updated package

--- src/UI/Server/CheckVersion.ps1
+++ src/UI/Server/CheckVersion.ps1
@@ -2,7 +2,6 @@ $server = "$env:AppUrl"
 $uri = "$server/version"
 $buildnumber = "$env:Version"
 Write-Host "Getting version $uri"
-Write-Host "Version should be: $buildnumber"
 Invoke-WebRequest $uri -UseBasicParsing | Foreach {
     $_.Content.Contains($buildnumber) | Foreach {
         if(-Not($_)) {
