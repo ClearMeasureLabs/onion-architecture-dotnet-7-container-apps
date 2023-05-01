c6196066c699bc0474009878a7b51081b7dab352
Dale Marttinen <dmarttinen@gmail.com>
2023-05-01 09:35:22 -0700

    corrected buildnumber var in checkversion script

--- CheckVersion.ps1
+++ CheckVersion.ps1
@@ -1,13 +1,14 @@
 $server = "$env:AppUrl"
 $uri = "$server/version"
+$buildnumber = "$env:buildnumber"
 Write-Host "Getting version $uri"
 Invoke-WebRequest $uri -UseBasicParsing | Foreach {
-    $_.Content.Contains($(Build.BuildNumber)) | Foreach {
+    $_.Content.Contains($buildnumber) | Foreach {
         if(-Not($_)) {
             Throw "Incorrect version."
         }
         else {
-            Write-Host "Correct version: $(Build.BuildNumber)"
+            Write-Host "Correct version: $buildnumber"
         }
     }
 }
