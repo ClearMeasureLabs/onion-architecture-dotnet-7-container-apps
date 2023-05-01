12a345f38beadb42fd8b1e92f8b396a2a6cc4712
Dale Marttinen <dmarttinen@gmail.com>
2023-05-01 13:54:44 -0700

    configured checkversion to take inputs

--- src/UI/Server/CheckVersion.ps1
+++ src/UI/Server/CheckVersion.ps1
@@ -1,14 +1,17 @@
-$server = "$env:AppUrl"
+param(
+    [str]$server,
+    [str]$version
+)
+
 $uri = "$server/version"
-$buildnumber = "$env:Version"
 Write-Host "Getting version $uri"
 Invoke-WebRequest $uri -UseBasicParsing | Foreach {
-    $_.Content.Contains($buildnumber) | Foreach {
+    $_.Content.Contains($version) | Foreach {
         if(-Not($_)) {
             Throw "Incorrect version."
         }
         else {
-            Write-Host "Correct version: $buildnumber"
+            Write-Host "Correct version: $version"
         }
     }
 }
