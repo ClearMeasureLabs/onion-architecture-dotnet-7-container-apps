c76bf433b9610a7f5896531a91f4476d18f7f492
Dale Marttinen <dmarttinen@gmail.com>
2023-05-01 14:45:43 -0700

    updated pathing

--- src/Database/scripts/UpdateAzureSql.ps1
+++ src/Database/scripts/UpdateAzureSql.ps1
@@ -8,6 +8,7 @@ $DatabaseUser = $OctopusParameters["DatabaseUser"]
 $DatabasePassword = $OctopusParameters["DatabasePassword"]
 Write-Output "Recursive directory listing for diagnostics"
 Get-ChildItem -Recurse
+Set-Location ..
 Write-Host "Executing & .\scripts\AliaSQL.exe $DatabaseAction $databaseServer $databaseName .\scripts $databaseUser $databasePassword"
 & .\scripts\AliaSQL.exe $DatabaseAction $DatabaseServer $DatabaseName .\scripts $DatabaseUser $DatabasePassword
 if ($lastexitcode -ne 0) {
