a220a25e4f0fba22de7100749b5cd8c84a3c4d75
Dale Marttinen <dmarttinen@gmail.com>
2023-08-22 09:22:19 -0700

    added run_runbook script

---
+++ run_runbook.ps1
@@ -0,0 +1,27 @@
+$ErrorActionPreference = "Stop";
+
+# Define working variables
+$octopusURL = "https://clearmeasure.octopus.app/"
+$octopusAPIKey = "API-L5IB8Y1CRCZPXSU1D33DO6WFE8FNWMXU"
+$header = @{ "X-Octopus-ApiKey" = $octopusAPIKey }
+$spaceName = "Onion DevOps"
+$projectName = "onion-architecture-dotnet-7-container-apps"
+$runbookName = "Unhealthy app alert"
+$environmentNames = @("TDD")
+
+# Get space
+$space = (Invoke-RestMethod -Method Get -Uri "$octopusURL/api/spaces/all" -Headers $header) | Where-Object {$_.Name -eq $spaceName}
+Write-Host "Using Space named $($space.Name) with id $($space.Id)"
+
+# Create the release body
+$createRunbookRunCommandV1 = @{
+	SpaceId          = $space.Id
+    SpaceIdOrName    = $spaceName
+    ProjectName      = $projectName
+    RunbookName      = $runbookName
+    EnvironmentNames = $environmentNames
+} | ConvertTo-Json
+
+# Run runbook
+Invoke-RestMethod -Method POST -Uri "$OctopusURL/api/$($Space.Id)/runbook-runs/create/v1" -Body $createRunbookRunCommandV1 -Headers $header
+
