06b0dbaf83368a637bce58cad78a67fc4c96f885
Richard Ram <richard.ram@clear-measure.com>
2024-01-03 13:57:19 -0800

    feat(maui) - added new maui project

---
+++ src/UI/Maui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace UI.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/UI/Maui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace UI.Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
