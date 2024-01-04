951447b57b570cbca09dd6d6831c9903ac1da0e3
Richard Ram <richard.ram@clear-measure.com>
2024-01-04 11:41:10 -0800

    feat(maui) - Copied pages from UI.Client and set up local rest api config for andriod emulator

--- src/UI/Maui/MauiProgram.cs
+++ src/UI/Maui/MauiProgram.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Palermo.BlazorMvc;
 
 namespace UI.Maui
 {
@@ -15,9 +17,20 @@ namespace UI.Maui
                 });
 
             builder.Services.AddMauiBlazorWebView();
+            builder.Services.AddScoped<IUiBus>(provider => new MvcBus(NullLogger<MvcBus>.Instance));
+
+            var handler = new Xamarin.Android.Net.AndroidMessageHandler();
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) =>
+            {
+                if (cert != null && cert.Issuer.Equals("CN=localhost"))
+                    return true;
+                return errors == System.Net.Security.SslPolicyErrors.None;
+            };
+
+            builder.Services.AddSingleton(sp => new HttpClient(handler) { BaseAddress = new Uri("https://10.0.2.2:7174/") });
 
 #if DEBUG
-    		builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Services.AddBlazorWebViewDeveloperTools();
     		builder.Logging.AddDebug();
 #endif
 
---
+++ src/UI/Maui/Pages/FetchDataController.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json;
+using Palermo.BlazorMvc;
+using ProgrammingWithPalermo.ChurchBulletin.Core.Model;
+using static System.Net.WebRequestMethods;
+
+namespace UI.Maui.Pages;
+
+[Route("/fetchdata")]
+public class FetchDataController : ControllerComponentBase<FetchDataView>
+{
+    private WeatherForecast[]? _forecasts;
+    [Inject] public HttpClient? Http { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        Debug.Assert(Http != null, nameof(Http) + " != null");
+
+        
+
+        _forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("WeatherForecast");
+        View.Model = _forecasts;
+    }
+}
