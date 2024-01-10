65c6e525784d9651371d92917eb82e2db060cb1a
AzureAD\JonathanMarkman <jonathan.markman@clear-measure.com>
2024-01-10 18:19:13 -0500

    Copy over Blazor pages from MAUI project, update _Imports

---
+++ src/UI.Wpf/Pages/FetchDataController.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components;
+using Palermo.BlazorMvc;
+using ProgrammingWithPalermo.ChurchBulletin.Core.Model;
+
+namespace UI.Wpf.Pages;
+
+[Route("/fetchdata")]
+public class FetchDataController : ControllerComponentBase<FetchDataView>
+{
+    private WeatherForecast[]? _forecasts;
+    [Inject] public HttpClient? Http { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        Debug.Assert(Http != null, nameof(Http) + " != null");
+        _forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("WeatherForecast");
+        View.Model = _forecasts;
+    }
+}
